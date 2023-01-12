deddad4d9874b03c1bac3844715e3b36f3fb0f10
Prasham1710 <jainprasham17@gmail.com>
2023-01-12 23:31:04 +0530

    relative

--- src/components/Category.js
+++ src/components/Category.js
@@ -6,7 +6,17 @@ const Category = () => {
         <img className='absolute w-full h-[225px] py-1 object-cover  mix-blend-overlay opacity-40' src='https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' />
         
         <div className='flex justify-between py-4 px-10 '>
-        <div><img className='h-[150px] w-[150px] rounded-full' src='https://images.pexels.com/photos/2672979/pexels-photo-2672979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/'/><h1 className='font-bold pl-12 '>Fashion</h1></div>
+        <div><div className='flex flex-col my-2'><label><img className='h-[150px] w-[150px] rounded-full' src='https://images.pexels.com/photos/2672979/pexels-photo-2672979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/'/><h1 className='font-bold pl-12 '>Fashion</h1></label>
+                  
+                  <select className='border relative rounded-md p-1'>
+                      <option>Grande Antigua</option>
+                      <option>Key West</option>
+                      <option>Maldives</option>
+                      <option>Cozumel</option>
+                      <option>Jamaica</option>
+                      <option>Bora Bora</option>
+                  </select>
+              </div></div>
         <div><img className='h-[150px] w-[150px] rounded-full' src='https://images.pexels.com/photos/264636/pexels-photo-264636.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' /><h1 className='font-bold pl-10'>Grocery</h1></div>
         <div><img className='h-[150px] w-[150px] rounded-full' src='https://images.pexels.com/photos/47261/pexels-photo-47261.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' /><h1 className='font-bold pl-10'>Mobiles</h1></div>
         <div><img className='h-[150px] w-[150px] rounded-full' src='https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' /><h1 className='font-bold pl-9'>Electronics</h1></div>
