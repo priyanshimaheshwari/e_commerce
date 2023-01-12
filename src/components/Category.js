6082497ec4512968eabe40d5ad9a4b343cc338b9
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-01-12 17:03:08 +0530

    Category

--- src/components/Category.js
+++ src/components/Category.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Category = () => {
   return (
     <div>
-        
+        <h1 className='font-bold text-5xl h-[full] w-[full] justify-center'>Category</h1>
     </div>
   )
 }
