45de4054a30fc279b1b3659838e6e1000040db9e
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-01-13 02:23:25 +0530

    style

--- src/components/Finaltouch.js
+++ src/components/Finaltouch.js
@@ -8,7 +8,7 @@ const Shoe = () => {
                 Final Touches
             </div>
             <div className=''>
-                <img className='absolute w-full h-[300px] py-1 object-cover  mix-blend-overlay opacity-80' src='https://images.pexels.com/photos/1261728/pexels-photo-1261728.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' />
+                <img className='absolute w-full h-[300px] py-1 object-cover  mix-blend-overlay opacity-80' src='https://images.pexels.com/photos/844297/pexels-photo-844297.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' />
                 <div className='rounded-xl relative grid md:grid-cols-5 px-10'>
                      <div><img className='h-[260px] w-[250px] pt-6 shadow-lg hover:scale-105 duration-300' src='https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' /><h1 className='font-bold text-2xl text-white px-16'>Hoodie</h1></div>
                      <div><img className='h-[260px] w-[250px] pt-6 shadow-lg hover:scale-105 duration-300' src='https://images.pexels.com/photos/2463256/pexels-photo-2463256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' /><h1 className='font-bold text-2xl text-white px-16'>Shorts</h1></div>
