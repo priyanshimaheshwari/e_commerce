094f7bee8551ff22a46a1fff12e531bdf3673aba
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-01-13 02:15:59 +0530

    style

--- src/components/Kid.js
+++ src/components/Kid.js
@@ -8,7 +8,7 @@ const Kid = () => {
                Kid's fashion fiesta
             </div>
             <div className=''>
-                <img className='absolute w-full h-[300px] py-1 object-cover  mix-blend-overlay opacity-0' src='https://images.pexels.com/photos/35857/amazing-beautiful-breathtaking-clouds.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' />
+                <img className='absolute w-full h-[300px] py-1 object-cover  mix-blend-overlay opacity-60' src='https://images.pexels.com/photos/35857/amazing-beautiful-breathtaking-clouds.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' />
                 <div className='rounded-xl relative grid md:grid-cols-4 px-20'>
                      <div><img className='h-[260px] w-[250px] pt-6 shadow-lg hover:scale-105 duration-300' src='https://images.pexels.com/photos/1133721/pexels-photo-1133721.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' /><h1 className='font-bold text-2xl text-white px-16'>Dresses</h1></div>
                      <div><img className='h-[260px] w-[250px] pt-6 shadow-lg hover:scale-105 duration-300' src='https://images.pexels.com/photos/1650281/pexels-photo-1650281.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' /><h1 className='font-bold text-2xl text-white px-16'>Shirts</h1></div>
