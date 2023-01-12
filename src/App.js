68e0f46bd1e2fe9423bfe11f48bc8b70c53bfece
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-01-13 02:17:45 +0530

    shoe

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hero from './components/Hero'
 import Kid from './components/Kid'
 import Men from './components/Men'
 import Navbar from './components/Navbar'
+import Shoe from './components/Shoe'
 import Women from './components/Women'
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
       <Men />
       <Women />
       <Kid />
+      <Shoe />
       <Electronics />
     </div>
   )
