31b703a16e2bd6519fc9611b7a3f23393e56ab63
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-01-12 23:40:51 +0530

    Electronics

--- src/components/Category.js
+++ src/components/Category.js
@@ -22,7 +22,17 @@ const Category = () => {
         
         <div><img className='h-[150px] w-[150px] rounded-full' src='https://images.pexels.com/photos/264636/pexels-photo-264636.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' /><h1 className='font-bold pl-10'>Grocery</h1></div>
         <div><img className='h-[150px] w-[150px] rounded-full' src='https://images.pexels.com/photos/47261/pexels-photo-47261.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' /><h1 className='font-bold pl-10'>Mobiles</h1></div>
-        <div><img className='h-[150px] w-[150px] rounded-full' src='https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' /><h1 className='font-bold pl-9'>Electronics</h1></div>
+        <div><img className='h-[150px] w-[150px] rounded-full' src='https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' /><select className='relative '>
+          <option>Electronics</option>
+          <option>Audio</option>
+          <option>Gaming</option>
+          <option>Powerbank</option>
+          <option>Laptop and Desktop</option>
+          <option>Laptop Accessories</option>
+          <option>Mobile Accessories</option>
+          <option>Smart Wear</option>
+          <option>Personal and Health Care</option>
+          <option>Storage</option></select></div>
         <div><img className='h-[150px] w-[150px] rounded-full' src='https://images.pexels.com/photos/1148955/pexels-photo-1148955.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' /><h1 className='font-bold pl-12'>Home</h1></div>
         <div><img className='h-[150px] w-[150px] rounded-full' src='https://images.pexels.com/photos/1450903/pexels-photo-1450903.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' /><h1 className='font-bold pl-8'>Appliances</h1></div>
         <div><img className='h-[150px] w-[150px] rounded-full' src='https://images.pexels.com/photos/46148/aircraft-jet-landing-cloud-46148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' /><h1 className='font-bold pl-14'>Travel</h1></div>
