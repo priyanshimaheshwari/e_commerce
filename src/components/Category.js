8934727fd93c6e993ccfccd1a0b66ad1cf50ed40
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-01-12 18:08:18 +0530

    style

--- src/components/Category.js
+++ src/components/Category.js
@@ -8,7 +8,7 @@ const Category = () => {
         <img className=' h-[200px] w-[200px]' src='https://images.unsplash.com/photo-1584273143981-41c073dfe8f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHdvbWVucyUyMGNsb3RoaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60' alt='/'/>
         <img className='h-[200px] w-[200px]' src='https://images.pexels.com/photos/1306248/pexels-photo-1306248.jpeg?auto=compress&cs=tinysrgb&w=400' alt='/' />
         <img className='h-[200px] w-[200px]' src='https://images.pexels.com/photos/1620760/pexels-photo-1620760.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='' />
-        <img className='h-20 w-30' src='https://images.pexels.com/photos/325527/pexels-photo-325527.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' />
+        <img className='h-[200px] w-[200px]' src='https://images.pexels.com/photos/325527/pexels-photo-325527.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' />
         <img className='h-20 w-30' src='https://images.pexels.com/photos/1148955/pexels-photo-1148955.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' />
         <img className='h-20 w-30' src='https://images.pexels.com/photos/4108800/pexels-photo-4108800.jpeg?auto=compress&cs=tinysrgb&w=400' alt='/' />
         </div>
