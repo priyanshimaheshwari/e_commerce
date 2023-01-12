964162f541ea58c850b6df49982dc52e33af6641
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-01-12 18:12:51 +0530

    style

--- src/components/Category.js
+++ src/components/Category.js
@@ -6,8 +6,8 @@ const Category = () => {
         <h1 className='font-bold text-5xl h-[full] w-[full] '>Category</h1>
         <div className='flex justify-between py-4 px-10 '>
         <img className=' h-[250px] w-[200px] rounded-full' src='https://images.unsplash.com/photo-1584273143981-41c073dfe8f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHdvbWVucyUyMGNsb3RoaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60' alt='/'/>
-        <img className='h-[250px] w-[200px]' src='https://images.pexels.com/photos/1306248/pexels-photo-1306248.jpeg?auto=compress&cs=tinysrgb&w=400' alt='/' />
-        <img className='h-[250px] w-[200px]' src='https://images.pexels.com/photos/1620760/pexels-photo-1620760.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='' />
+        <img className='h-[250px] w-[200px] rounded-full' src='https://images.pexels.com/photos/1306248/pexels-photo-1306248.jpeg?auto=compress&cs=tinysrgb&w=400' alt='/' />
+        <img className='h-[250px] w-[200px] rounded-full' src='https://images.pexels.com/photos/1620760/pexels-photo-1620760.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='' />
         <img className='h-[250px] w-[200px]' src='https://images.pexels.com/photos/325527/pexels-photo-325527.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' />
         <img className='h-[250px] w-[200px]' src='https://images.pexels.com/photos/1148955/pexels-photo-1148955.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' />
         <img className='h-[250px] w-[200px]' src='https://images.pexels.com/photos/4108800/pexels-photo-4108800.jpeg?auto=compress&cs=tinysrgb&w=400' alt='/' />
