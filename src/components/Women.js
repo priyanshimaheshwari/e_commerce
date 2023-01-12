54f7f9ca389264eea9ee565d0232c6c9780f53c3
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-01-13 01:55:23 +0530

    style

--- src/components/Women.js
+++ src/components/Women.js
@@ -12,7 +12,7 @@ const Women = () => {
                 <div className='rounded-xl relative grid md:grid-cols-5 px-10'>
                      <div><img className='h-[260px] w-[250px] pt-6' src='https://images.pexels.com/photos/2235071/pexels-photo-2235071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' /><h1 className='font-bold text-2xl text-white px-16'>Dresses</h1></div>
                      <div><img className='h-[260px] w-[250px] pt-6' src='https://images.pexels.com/photos/13251953/pexels-photo-13251953.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' /><h1 className='font-bold text-2xl text-white px-16'>Tops</h1></div>
-                     <div><img className='h-[260px] w-[250px] pt-6' src='https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' /><h1 className='font-bold text-2xl text-white pl-20'>T-Shirts</h1></div>
+                     <div><img className='h-[260px] w-[250px] pt-6' src='https://images.pexels.com/photos/2820793/pexels-photo-2820793.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' /><h1 className='font-bold text-2xl text-white pl-20'>Skirts</h1></div>
                      <div><img className='h-[260px] w-[250px] pt-6' src='https://images.pexels.com/photos/1598507/pexels-photo-1598507.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' /><h1 className='font-bold text-2xl text-white pl-20'>Jeans</h1></div>
                      <div><img className='h-[260px] w-[250px] pt-6' src='https://images.pexels.com/photos/297933/pexels-photo-297933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' /><h1 className='font-bold text-2xl text-white pl-20'>Shirts</h1></div>
     
